c302489fc5c26428511d2491644c517c136b37be
pat0510 <prpo221479@upemor.edu.mx>
2024-11-20 11:07:21 -0600

    dispo

--- model/consultar_disponibilidades_a.php
+++ model/consultar_disponibilidades_a.php
@@ -3,24 +3,13 @@
 include_once "db.php";
 
 function obtenerDisponibilidades($conn) {
-    $sql = "SELECT 
-                d.idConductor,
-                u.nombre,
-                u.apellido,
-                d.dia,
-                d.horaInicio,
-                d.horaFin
-            FROM disponibilidad AS d
-            INNER JOIN usuarios AS u ON d.idConductor = u.id
-            WHERE u.tipo = 'conductor';";
-
+    $sql = "SELECT * FROM disponibilidad;";
     $exec = mysqli_query($conn, $sql);
-
+    
     if (!$exec) {
         die("Error en la consulta: " . mysqli_error($conn));
     }
-
+    
     return $exec;
 }
-
 ?>
